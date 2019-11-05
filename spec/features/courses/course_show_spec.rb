5c6caf21de15bca3d3bf8092c5edb95dc85978cd
Zac Isaacson <isaacson.zm@gmail.com>
2019-11-05 09:17:01 -0700

    Add test for unenrolling a student Create unenroll link in course show Create StudentCourses controller

--- spec/features/courses/course_show_spec.rb
+++ spec/features/courses/course_show_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Course show page' do
-  it "will show course name and sorted students" do
-
+  before (:each) do
     @zac = Student.create(name: "Zac Isaacson")
     @einstein = Student.create(name: "Albert Einstein")
     @turing = Student.create(name: "Alan Turing")
@@ -12,6 +11,8 @@ RSpec.describe 'Course show page' do
     StudentCourse.create(student_id: @zac.id, course_id: @calculus.id, grade: 90)
     StudentCourse.create(student_id: @einstein.id, course_id: @calculus.id, grade: 100)
     StudentCourse.create(student_id: @turing.id, course_id: @calculus.id, grade: 95)
+  end
+  it "will show course name and sorted students" do
 
     visit "/courses/#{@calculus.id}"
 
@@ -25,4 +26,21 @@ RSpec.describe 'Course show page' do
       expect(page).to have_content(@zac.name)
     end
   end
+
+  it "can unenroll a student" do
+    visit "/courses/#{@calculus.id}"
+
+    within '#student-0' do
+      click_link 'Unenroll'
+    end
+
+    expect(current_path).to eq("/students/#{@einstein.id}")
+
+    expect(page).to_not have_content('Calculus')
+  end
+# As a user,
+# When I visit a course show page,
+# Then I see a button 'unenroll' next to each student's name
+# When I click this button,
+# Then I am redirected to that student's show page where I do not see the course listed
 end
